2de02cf8d18e538233c66025afa398b39381c3db
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2024-10-21 14:08:21 +0500

    повтор

---
+++ Task03/Task03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 150 (включительно)");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 150)
+            {
+                Console.WriteLine("Неверный ввод. Введите число от 1 до 150!");
+                Console.ReadKey();
+            }
+
+
+            for (int i = 101; i <= 150; i++)
+            {
+                for (int j = 0; j < i.ToString().Length; j++)
+                {
+                    char digit = i.ToString()[j];
+                    if (j == number)
+
+                    {
+                        Console.WriteLine("{digit}");
+                        
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
