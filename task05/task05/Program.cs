1f087dd8aa69d0ede50102a21755f52d4d7e9730
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2024-10-22 13:28:27 +0500

    Задание 5. решено

---
+++ task05/task05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            double y = F(2, 3, 5) + F(3, 4, 7) + F(5, 6, 11);
+
+
+
+            Console.WriteLine("x = " + Math.Round(y, 3));
+
+            Console.ReadKey();
+        }
+
+        static double F(double a, double b, double c)
+        {
+            return ((Math.Sin(a) - Math.Cos(b)) / Math.Sqrt(c));
+        }
+    }
+}
---
+++ task05/task05/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("task05")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ZverDVD")]
+[assembly: AssemblyProduct("task05")]
+[assembly: AssemblyCopyright("Copyright © ZverDVD 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("75d9efaa-a4b4-4097-a64f-b627623e95fe")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
