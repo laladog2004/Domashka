781757bb5fd562d7be50a1e97437aba49d6120d6
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2024-10-22 13:55:14 +0500

    Задание 6.1. решено

---
+++ task06.1/task06.1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст на русском языке");
+            var text = Console.ReadLine();
+
+            Console.WriteLine("\nНа алфавите CS:");
+            Console.WriteLine(CSTranslate(text));
+
+            Console.ReadKey();
+        }
+
+        static string CSTranslate(string s)
+        {
+            return s
+                .ToUpper()
+                .Replace(" ", "  ")
+                .Replace("А", "A")
+                .Replace("Б", "6")
+                .Replace("В", "B")
+                .Replace("Г", "r")
+                .Replace("Д", "D")
+                .Replace("Е", "E")
+                .Replace("Ё", "E")
+                .Replace("Ж", "}|{")
+                .Replace("З", "3")
+                .Replace("И", "u")
+                .Replace("Й", "u*")
+                .Replace("К", "K")
+                .Replace("Л", "JI")
+                .Replace("М", "M")
+                .Replace("Н", "H")
+                .Replace("О", "O")
+                .Replace("П", "n")
+                .Replace("Р", "P")
+                .Replace("С", "C")
+                .Replace("Т", "T")
+                .Replace("У", "Y")
+                .Replace("Ф", "cp")
+                .Replace("Х", "X")
+                .Replace("Ц", "L|")
+                .Replace("Ч", "4")
+                .Replace("Ш", "LLI")
+                .Replace("Щ", "LLL")
+                .Replace("Ъ", "`b")
+                .Replace("Ы", "b|")
+                .Replace("Ь", "b")
+                .Replace("Э", "-)")
+                .Replace("Ю", "IO")
+                .Replace("Я", "9I")
+
+                ;
+        }
+    }
+}
