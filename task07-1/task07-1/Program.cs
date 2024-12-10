d0e09be8d18b001573e5d123a5361cc0b63c690f
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2024-12-10 11:44:50 +0500

    Задание 07-1 выполнено

---
+++ task07-1/task07-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("12. Только одно из числе m и n меньше двадцати.");
+            var m = GetNumber("m");
+            var n = GetNumber("n");
+
+            if (IsStatementTrue(m, n))
+                Console.WriteLine("Утверждение истинно");
+            else
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+
+        }
+        static bool IsStatementTrue(int m, int n)
+        {
+            if (m < 20 && n > 20 || n < 20 && m > 20)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static int GetNumber(string numberName)
+        {
+            
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
