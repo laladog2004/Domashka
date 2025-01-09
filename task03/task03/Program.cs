411eb80f343fe3cb54478a00023313ba54cad8dc
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2025-01-09 20:23:36 +0500

    Задание 3 выполнено. Вариант 12

---
+++ task03/task03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число k (1 <= k <= 150): ");
+            int k = int.Parse(Console.ReadLine());
+
+            string sequence = "";
+
+            for (int i = 101; i <= 150; i++)
+            {
+                sequence += i.ToString();
+            }
+
+            Console.WriteLine($"Цифра на позиции {k} в последовательности: {sequence[k - 1]}");
+
+            Console.ReadKey();
+        }
+    }
+}
