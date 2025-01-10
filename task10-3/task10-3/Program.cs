a334cc2ee0decaaedb71d15bdfe5e311b428dfdb
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2025-01-10 16:34:06 +0500

    Задание 10-3 выполнено.

---
+++ task10-3/task10-3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+
+            Console.WriteLine("Введите число a (1 < a <= 2)");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out a))
+                {
+                    if (a > 1 && a <= 2)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Число должно быть больше 1 и не больше 2. Попробуйте снова.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода. Введите число.");
+                }
+            }
+
+
+            bool first = true;
+            for (int n = 1; ; n++)
+            {
+                double currentValue = 1 + 1 / Math.Sqrt(n);
+
+                if (currentValue < a)
+                {
+                    continue;
+                }
+
+                if (!first)
+                    Console.Write("; ");
+                Console.Write($"1 + 1/√{n}");
+
+                first = false;
+            }
+
+
+        }
+
+        
+    }
+
+}
