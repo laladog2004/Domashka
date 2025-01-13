2a70b2f8e2eb7a3876d2019b41b5c29e6672221a
Varvara.Lenkova@urfu.me <Varvara.Lenkova@urfu.me>
2025-01-13 12:15:44 +0500

    Задание 10-5 выполнено. Вариант 12

---
+++ task10-5/task10-5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число:\n"
+            + "(Не менее двух знаков!)");
+            string input = Console.ReadLine();
+
+            if (long.TryParse(input, out long number) && number >= 10)
+            {
+                char[] digits = input.ToCharArray();
+
+                int left = 0;
+                int right = digits.Length - 1;
+
+                while (left < right)
+                {
+                    char temp = digits[left];
+                    digits[left] = digits[right];
+                    digits[right] = temp;
+
+                    left++;
+                    right--;
+                }
+
+                string result = new string(digits);
+                Console.WriteLine("Измененное число: " + result);
+                Console.ReadKey(); 
+            }
+            else
+            {
+                Console.WriteLine("Введено некорректное число.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
